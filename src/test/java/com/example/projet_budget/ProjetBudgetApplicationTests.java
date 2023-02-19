b955cc8a67e5e402060628c36d84c98c2b6c02d9
Assmaegithub <alidaassmae@gmail.com>
2023-02-19 19:50:54 +0100

    init

---
+++ src/test/java/com/example/projet_budget/ProjetBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
