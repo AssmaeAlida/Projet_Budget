0464e3333233c9ec8576080f16e37331fa7ee0d9
manalalm <manallamaallem@gmail.com>
2023-03-04 12:46:00 +0100

    add

---
+++ src/main/java/com/example/projet_budget/ws/CategorieEntiteAdministratifWs.java
@@ -0,0 +1,33 @@
+package com.example.projet_budget.Ws;
+
+import com.example.projet_budget.Bean.CategorieEntiteAdministratif;
+import com.example.projet_budget.Service.CategorieEntiteAdministratifService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categorie-entite")
+public class CategorieEntiteAdministratifWs {
+    @Autowired
+    private CategorieEntiteAdministratifService categorieEntiteAdministratifService;
+    @GetMapping("/code/{code}")
+    public CategorieEntiteAdministratif findByCode(@PathVariable String code) {
+        return categorieEntiteAdministratifService.findByCode(code);
+    }
+    @DeleteMapping ("/code/{code}")
+    @Transactional
+    public int deleteByCode(@PathVariable String code) {
+        return categorieEntiteAdministratifService.deleteByCode(code);
+    }
+    @GetMapping("categorieEntite/code/{code}")
+    public List<CategorieEntiteAdministratif> findByCategorieEntiteAdministratifCode(@PathVariable String code) {
+        return categorieEntiteAdministratifService.findByCategorieEntiteCode(code);
+    }
+    @PostMapping ("/")
+    public int save(@RequestBody CategorieEntiteAdministratif categorieEntiteAdministratif) {
+        return categorieEntiteAdministratifService.save(categorieEntiteAdministratif);
+    }
+}
