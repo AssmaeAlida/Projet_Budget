42eed3f23598a1f47bd3186e13d7e436347aeb14
manalalm <manallamaallem@gmail.com>
2023-04-07 04:01:37 +0200

    pom

--- src/main/java/com/example/projetBudget/ProjetBudgetApplication.java
+++ src/main/java/com/example/projetBudget/ProjetBudgetApplication.java
@@ -1,17 +1,10 @@
 package com.example.projetBudget;
-
-import com.example.projetBudget.security.bean.Role;
-import com.example.projetBudget.security.bean.User;
-import com.example.projetBudget.security.service.facade.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
 
 @SpringBootApplication
-public class ProjetBudgetApplication implements CommandLineRunner {
+public class ProjetBudgetApplication  {
 
     public static void main(String[] args) {
 
@@ -20,19 +13,6 @@ public class ProjetBudgetApplication implements CommandLineRunner {
 
 
 
-    @Override
-    public void run(String... args) throws Exception {
-        if(true){
-            User admin = new User("admin","admin");
-            admin.setAuthorities(Arrays.asList(new Role("ROLE_ADMIN")));
-            userService.save(admin);
 
-            User chef = new User("chef","chef");
-            chef.setAuthorities(Arrays.asList(new Role("ROLE_CHEF")));
-            userService.save(chef);
-        }
 
-    }
-    @Autowired
-    private UserService userService;
 }
--- src/main/java/com/example/projetBudget/security/service/util/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetBudget.security.service.util;
-
-public class StringUtil {
-    public static boolean isNotEmpty(Object value) {
-        return value!= null && !value.toString().isEmpty();
-    }
-}
